f3e779ce30c32605504280be7435515b720b4299
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-10-23 18:58:27 +0300

    Add files via upload

---
+++ Proga/cmake-build-debug/hw4/CMakeFiles/hw4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw4.dir/main.cpp.obj"
+  "hw4.exe"
+  "hw4.exe.manifest"
+  "hw4.pdb"
+  "libhw4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
