1b5769eb8b659ec104f5d8ddd2272278e9d1623e
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-10-23 19:01:18 +0300

    Add files via upload

---
+++ Proga/cmake-build-debug/CMakeFiles/polytech.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/polytech.dir/main.cpp.obj"
+  "libpolytech.dll.a"
+  "polytech.exe"
+  "polytech.exe.manifest"
+  "polytech.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/polytech.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
