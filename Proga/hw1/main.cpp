8b09cc01b26dc106e221a0f59b193480b592dd80
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-10-23 18:52:35 +0300

    Add files via upload

---
+++ Proga/hw1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int fact(int n){
+    return (n==1)?1:n*fact(n-1);
+}
+int main() {
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
