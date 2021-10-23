22423019b54eea7346688ee87126ad78d15b8d6e
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-10-23 15:05:44 +0300

    Add files via upload

---
+++ Proga/Task 2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int fact(int n){
+    return (n==0)?1:(n==1)?1:n*fact(n-1);
+}
+int main() {
+    int n,m;
+    cout<<"enter the number n: ";
+    cin>>n;
+    cout<<"enter the number m: ";
+    cin>>m;
+    if (m > n){
+        cout<<"incorrect m and n";
+        return 0;
+    }
+    cout<<fact(n)/(fact(n-m)*fact(m));
+    return 0;
+}
