664a22333baa1e286433451dbb3e65ce9239537b
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-11-20 21:34:50 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -9,18 +9,18 @@ int main()
     string path = "in.txt";
     ifstream fin; // для считывания данных из файла, а ofstream для создания файла или ввода данных
     fin.open(path);
-//     if (!fin.is_open())
-//     {
-//         cout<<"File open error!"<<endl;
-//     }
-//     else
-//     {
-//         cout << "File is open!" << endl;
+    if (!fin.is_open())
+    {
+//        cout<<"File open error!"<<endl;
+    }
+    else
+    {
+//        cout << "File is open!" << endl;
         int x_0, y_0;
         fin>>x_0>>y_0;                            // переменные начальные для работы с данными из файла
         int x,y;                                  // переменные дял записи координат
-        int xL = 0, yL = 0, xR = 0, yR = 0;                          // переменные для наиболее левых и правых координат
-        double cosR = 1, cosL = 1;                     // начальные косинусы при 0 градусах
+        int xL,yL,xR,yR;                          // переменные для наиболее левых и правых координат
+        double cosR=1,cosL=1;                     // начальные косинусы при 0 градусах
         while(fin>>x>>y)
         {
             double cos = (x*x_0 + y*y_0)/(sqrt(x*x+y*y)*sqrt(x_0*x_0+y_0*y_0)); // cos угла между напр. вект. и следующими
@@ -38,9 +38,9 @@ int main()
                 yL = y;
             }
         }
-        cout << "Leftmost:: "<< xL <<" " << yL<<endl;
+        cout << "Leftmost:: "<< xL <<" " << yL <<endl;
         cout << "Rightmost: " << xR << " " << yR << endl;
-//     }
-//     fin.close();
-//     return 0;
+    }
+    fin.close();
+    return 0;
 }
