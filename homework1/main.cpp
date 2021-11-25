fbc4649fbb3fa1af18f91c9c8156d673434d6a07
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-11-25 19:33:38 +0300

    Update main.cpp

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -20,12 +20,13 @@ int main()
         fin>>x_0>>y_0;                            // переменные начальные для работы с данными из файла
         int x,y;                                  // переменные дял записи координат
         int xL = 0, yL = 0, xR = 0, yR = 0;                          // переменные для наиболее левых и правых координат
-        double cosR=1,cosL=1;                     // начальные косинусы при 0 градусах
+        double cosR,cosL;                     // начальные косинусы при 0 градусах
+        cosR = 1; cosL = 1;
         while(fin>>x>>y)
         {
             double cos = double((x*x_0 + y*y_0)) /double((sqrt(x*x+y*y)*sqrt(x_0*x_0+y_0*y_0))) ; // cos угла между напр. вект. и следующими
             cos =round(cos * 1000.0) / 1000.0;
-            if(cos < cosR && ((y_0*x) >= (y*x_0) ))
+            if(cos <= cosR && ((y_0*x) >= (y*x_0) ))
             {
                 cosR = cos;
                 xR = x;
@@ -38,7 +39,7 @@ int main()
                 yL = y;
             }
         }
-        cout << "Leftmost: "<< xL <<" " << yL <<endl;
+        cout << "Leftmost: " << xL <<" " << yL <<endl;
         cout << "Rightmost: " << xR << " " << yR << endl;
     }
     fin.close();
