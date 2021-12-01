9861d375574dac079b65b68a3d98e115e9a7510c
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-12-01 23:04:18 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -14,9 +14,9 @@ int main(int argc, char** argv)
     vector<double> X;
     vector<double> Y;
 
-    string path = argv[1};
+   // string path = argv[1};
     ifstream fin;
-    fin.open(path);
+    fin.open(argv[1]);
 
     if (fin.is_open()) {
 
