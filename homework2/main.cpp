7cab4853fadde43f90fad280d7b334820fc6dd0b
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-12-02 00:33:53 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -42,18 +42,18 @@ int main(int argc, char** argv)
     int direction = 1;
     double t = (vy + sqrt(vy*vy+2*y0*g))/g;
     double t_col;
-    double A_x = vx, A_y = vy, B_x = x0, B_y = y0;
+ 
 
     for (int i = 0; (i >= 0) && (i <= X.size()-1); i = i + direction)
     {
     // x = x0+vx*t
-        t_col = (X[i] - B_x)/A_x;
+        t_col = (X[i] - x0)/vx;
         if (t_col <= t)
         {
-            if ((B_y + A_y * t_col - g * t_col * t_col / 2 <= Y[i])) //y = y0 + vy*t -gt^2/2
+            if ((y0 + vy * t_col - g * t_col * t_col / 2 <= Y[i])) //y = y0 + vy*t -gt^2/2
             {
-                B_x = 2 * A_x * t_col + B_x;
-                A_x = -A_x;
+                x0 = 2 * vx * t_col + x0;
+                vx = -vx;
                 direction = (-1) * direction;
             }
         }
@@ -63,9 +63,9 @@ int main(int argc, char** argv)
             }
         }
 
-double XX = B_x + A_x*t; // поиск конечной координаты
+double XX = x0 + vx*t; // поиск конечной координаты
 
-for (int i=0; i <= X.size();i++)
+for (int i=0; i <= X.size()-1;i++)
 {
     if ((XX >= X[i]) && (XX <= X[i+1]))
     {
