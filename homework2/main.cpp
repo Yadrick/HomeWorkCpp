1568a7a8e9ef6d930d7fcf017bc64924dc329f9a
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-12-05 21:27:05 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -8,41 +8,77 @@ using namespace std;
 int main(int argc, char** argv)
 {
     double y0=0, x0 = 0, vx = 0, vy = 0, g = 9.8, x, y;
-    int site = 0;
+    double  check, tmp;
+    int site = 0, n = 0;
 
     vector<double> X;
     vector<double> Y;
+    X.push_back(0);
+//    ifstream fin("in.txt");
+//
+//    if (fin.is_open()) {
+//
+//        fin >> y0 >> vx >> vy;
+  //      double t = (vy + sqrt(vy * vy + 2 * y0 * g)) / g;
 
-    ifstream fin(argv[1]);
 
-    if (fin.is_open()) {
+      std::ifstream in_prev("in.txt");
+    if (in_prev.is_open()){
+        in_prev >> y0;
+        in_prev >> vx;
+        in_prev >> vy;
+        double t_end = (vy+sqrt(vy*vy+2*g*y0))/g;
+        while ((!in_prev.eof()) && (check <= vx*t_end)) {
+            while (in_prev >> check >> tmp) {
+                n++;
+            }
+        }
+    }
+    in_prev.close();
 
-        fin >> y0 >> vx >> vy;
-        double t = (vy + sqrt(vy * vy + 2 * y0 * g)) / g;
 
-        while ((fin >> x >> y))
-        {
+    std::ifstream in("in.txt");
+    if (in.is_open()){
+        in >> y0;
+        in >> vx;
+        in >> vy;
+        int i = 0;
+        double t_end = (vy+sqrt(vy*vy+2*g*y0))/g;
+        while ((!in.eof()) && (X[i - 1] <= vx*t_end) && (i <= n - 1)) {
+            in >> x;
+            in >> y;
             X.push_back(x);
             Y.push_back(y);
-            // for для того, чтобы избавиться от пробелов в конце in.txt
-//            for (int i = 0; i < X.size(); i++ )
-//            {
-//                if ((X[i] == X[i+1]) && (Y[i] == Y[i+1]))
-//                {
-//                    X.pop_back();
-//                    Y.pop_back();
-//                }
-//            }
+            i++;
         }
-
     }
-    fin.close();
+    in.close();
+
+X.erase(X.begin());
+
+//        while ((fin >> x >> y))
+//        {
+//            X.push_back(x);
+//            Y.push_back(y);
+//
+//            // for для того, чтобы избавиться от пробелов в конце in.txt
+////            for (int i = 0; i < X.size(); i++ )
+////            {
+////                if ((X[i] == X[i+1]) && (Y[i] == Y[i+1]))
+////                {
+////                    X.pop_back();
+////                    Y.pop_back();
+////                }
+////            }
+//        }
+//    }
+//    fin.close();
 
     int direction = 1;
     double t = (vy + sqrt(vy*vy+2*y0*g))/g;
     double t_col;
 
-    for (int i = 0; (i >= 0) && (i <= X.size()-1); i = i + direction)
+    for (int i = 0; (i >= 0) && (i <= X.size()-1) ; i = i + direction)
     {
         // x = x0+vx*t
         t_col = (X[i] - x0)/vx;
@@ -57,7 +93,7 @@ int main(int argc, char** argv)
         }
         else
         {
-            break;
+            break ;
         }
     }
 
