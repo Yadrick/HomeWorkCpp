e92ce11ca2c9d35b45f951b0003e5624682c4105
Yadrick <90789479+Yadrick@users.noreply.github.com>
2021-12-06 23:08:37 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -8,85 +8,70 @@ using namespace std;
 int main(int argc, char** argv)
 {
     double y0=0, x0 = 0, vx = 0, vy = 0, g = 9.8, x, y;
-    double distance = 0;
-    int site = 0, n = 0;
+    int site = 0;
 
     vector<double> X;
     vector<double> Y;
 
     ifstream fin(argv[1]);
-//  fin >> y0 >> vx >> vy;
-//while (fin>>x>>y){
-//    X.push_back(x);
-//    Y.push_back(y);
-//}
+
     if (fin.is_open()) {
         fin >> y0 >> vx >> vy;
-        double t = (vy + sqrt(vy * vy + 2 * y0 * g)) / g;
+        //double t = (vy + sqrt(vy * vy + 2 * y0 * g)) / g;
 
-        while ((fin >> x >> y) && (distance <= vx*t))
-        {
-            n++;
-        }
+            while (!fin.eof())
+            {
+                fin >> x >> y;
+                X.push_back(x);
+                Y.push_back(y);
+            }
 
-        int i = 0;
-        X.push_back(0);
-        fin.clear();
-        fin.seekg(0,ios::beg);
-        fin >> y0 >> vx >> vy;
 
-        while (fin >> x >> y) {
-            X.push_back(x);
-            Y.push_back(y);
-        }
     }
     fin.close();
-    X.erase(X.begin());
 
     int direction = 1;
     double t = (vy + sqrt(vy*vy+2*y0*g))/g;
     double t_col;
 
-    for (int i = 0; (i >= 0) && (i <= n-1) ; i = i + direction)
-    {
-        // x = x0+vx*t
-        t_col = (X[i] - x0)/vx;
-        if (t_col <= t)
-        {
-            if ((y0 + vy * t_col - g * t_col * t_col / 2 <= Y[i])) //y = y0 + vy*t -gt^2/2
-            {
-                x0 = 2 * vx * t_col + x0;
-                vx = -vx;
-                direction = (-1) * direction;
-            }
-        }
-        else
-        {
-            break ;
-        }
-    }
 
-    double XX = x0 + vx*t; // поиск конечной координаты
-
-    for (int i=0; (i <= n-1);i++)
-    {
-        if ((XX >= X[i]) && (XX <= X[i+1]))
-        {
-            site = i+1;
-            break;
-        }
-    }
-
-    if((XX > X[X.size()-1]))
-    {
-        site = X.size();
-    }
-
-    if(XX < X[0])
-    {
+    if (X.size() == 0) {
         site = 0;
     }
+    else {
+        for (int i = 0; (i >= 0) && (i <= X.size() - 1); i = i + direction) {
+            // x = x0+vx*t
+            t_col = (X[i] - x0) / vx;
+            if (t_col <= t) {
+                if ((y0 + vy * t_col - g * t_col * t_col / 2 <= Y[i])) //y = y0 + vy*t -gt^2/2
+                {
+                    x0 = 2 * vx * t_col + x0;
+                    vx = -vx;
+                    direction = (-1) * direction;
+                }
+            } else {
+                break;
+            }
+        }
 
+        double XX = x0 + vx * t; // поиск конечной координаты
+
+
+        for (int i = 0; (i <= X.size() - 1); i++) {
+            if ((XX >= X[i]) && (XX <= X[i + 1])) {
+                site = i + 1;
+                break;
+            }
+        }
+
+        if ((XX > X[X.size() - 1])) {
+            site = X.size();
+        }
+
+        if (XX < X[0]) {
+            site = 0;
+        }
+    }
 
     X.clear();
     Y.clear();
